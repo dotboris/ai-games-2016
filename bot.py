751085638c3b0c71267a31dab314d517a9f87878
Boris Bera <bera.boris@gmail.com>
2016-05-28 11:39:46 -0400

    implemented shortest path

--- bot.py
+++ bot.py
@@ -4,10 +4,9 @@ from pathfinding import navigate_towards, shortest_path
 
 
 class Bot:
-    def freemine(self, mines_locs):
-        for loc, hero in mines_locs.items():
-            if hero == "-":
-              return loc
+    def freemine(self, game, mines_locs):
+        untaken_mines = [loc for loc, hero in mines_locs.items() if hero == '-']
+        return sorted(untaken_mines, key=lambda l: len(shortest_path(game.board, game.hero.pos, l)))[0]
 
     def move(self, state):
         game = Game(state)
@@ -16,6 +15,6 @@ class Bot:
         # Pathfinding example:
         # dir = navigate_towards(game.board, game.hero.pos, (0, 0))
         dirs = ['Stay', 'North', 'South', 'East', 'West']
-        dest = self.freemine(game.mines_locs)
+        dest = self.freemine(game, game.mines_locs)
         print('dest is ' +  repr(dest))
         return navigate_towards(game.board, game.hero.pos, dest)
