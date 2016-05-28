91cfd94f1d127eb4cd2171a9de768dfa35603c52
Boris Bera <bera.boris@gmail.com>
2016-05-28 11:53:08 -0400

    Added browser black magic

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 from random import choice
 from game import Game
 from pathfinding import navigate_towards, shortest_path
+import webbrowser
 
 
 class Bot:
+    def __init__(self):
+        self.viewUrl = None
+        
     def freemine(self, game, mines_locs):
         untaken_mines = [loc for loc, hero in mines_locs.items() if hero == '-']
         return sorted(untaken_mines, key=lambda l: len(shortest_path(game.board, game.hero.pos, l)))[0]
 
     def move(self, state):
+        if not self.viewUrl:
+            self.viewUrl = state['viewUrl']
+            webbrowser.open(self.viewUrl,new=2)
+
         game = Game(state)
-        game.board.disp()
         # TODO implement SkyNet here
         # Pathfinding example:
         # dir = navigate_towards(game.board, game.hero.pos, (0, 0))
